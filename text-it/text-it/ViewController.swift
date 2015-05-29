f637b257bf29d3281f5bf82b35efaae602967ecb
Mertcan Yigin <mertcan.yigin@tum.de>
2015-05-29 14:10:52 +0200

    open an empty project

---
+++ text-it/text-it/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  text-it
+//
+//  Created by Mertcan Yigin on 5/29/15.
+//  Copyright (c) 2015 Mertcan Yigin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
